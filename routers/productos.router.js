64dc8f3c02dd74e072e6977e5b869342e8a7b61c
Matias Casiba <casibagabriel@gmail.com>
2025-04-29 11:03:59 -0300

    Ajustes en los routers

--- routers/productos.router.js
+++ routers/productos.router.js
@@ -5,18 +5,18 @@ import productosController from '../controllers/productos.controller.js'
 const routerProductos = express.Router()
 
 // ! CRUD Productos
-// CRUD -> R:READ ALL -> GET ALL -> http://localhost:8080/api/v1/productos
+// CRUD -> R:READ ALL -> GET ALL -> http://localhost:.../api/v1/productos
 routerProductos.get('/', productosController.getAll)
-// CRUD -> R:READ ONE -> GET ONE -> http://localhost:8080/api/v1/productos/id
+// CRUD -> R:READ ONE -> GET ONE -> http://localhost:.../api/v1/productos/id
 routerProductos.get('/:id', productosController.getOne)
   
-// CRUD -> C:CREATE -> POST -> http://localhost:8080/api/v1/productos + productoACrear
+// CRUD -> C:CREATE -> POST -> http://localhost:.../api/v1/productos + productoACrear
 routerProductos.post('/', productosController.create)
   
-// CRUD -> U:UPDATE -> PUT -> http://localhost:8080/api/v1/productos/id + productoAEditar
+// CRUD -> U:UPDATE -> PUT -> http://localhost:.../api/v1/productos/id + productoAEditar
 routerProductos.put('/:id', productosController.update)
   
-// CRUD -> D:DELETE -> DELETE -> http://localhost:8080/api/v1/productos/id
+// CRUD -> D:DELETE -> DELETE -> http://localhost:.../api/v1/productos/id
 routerProductos.delete('/:id', productosController.remove)
 
 export default routerProductos
