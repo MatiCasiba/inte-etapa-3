3801112b5593434bd95cebba95b34b25a94a1387
Matias Casiba <casibagabriel@gmail.com>
2025-04-29 11:09:42 -0300

    Ajusto nuevamente el router

--- routers/usuarios.router.js
+++ routers/usuarios.router.js
@@ -4,18 +4,18 @@ import usuariosController from '../controllers/usuarios.controller.js'
 
 
 // ! CRUD Usuarios
-// CRUD -> R:READ ALL -> GET ALL -> http://localhost:8080/api/v1/usuarios
+// CRUD -> R:READ ALL -> GET ALL -> http://localhost:.../api/v1/usuarios
 routerUsuarios.get('/', usuariosController.getAll)
-// CRUD -> R:READ ONE -> GET ONE -> http://localhost:8080/api/v1/usuarios/id
+// CRUD -> R:READ ONE -> GET ONE -> http://localhost:.../api/v1/usuarios/id
 routerUsuarios.get('/:id', usuariosController.getOne)
   
-// CRUD -> C:CREATE -> POST -> http://localhost:8080/api/v1/usuarios + usuarioACrear
+// CRUD -> C:CREATE -> POST -> http://localhost:.../api/v1/usuarios + usuarioACrear
 routerUsuarios.post('/', usuariosController.create)
   
-// CRUD -> U:UPDATE -> PUT -> http://localhost:8080/api/v1/usuarios/id + usuarioAEditar
+// CRUD -> U:UPDATE -> PUT -> http://localhost:.../api/v1/usuarios/id + usuarioAEditar
 routerUsuarios.put('/:id', usuariosController.update)
   
-// CRUD -> D:DELETE -> DELETE -> http://localhost:8080/api/v1/usuarios/id
+// CRUD -> D:DELETE -> DELETE -> http://localhost:.../api/v1/usuarios/id
 routerUsuarios.delete('/:id', usuariosController.remove)
 
 export default routerUsuarios
